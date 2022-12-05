c2ec3b0bf1eea5619ce64b1f21717a685b3fa318
Talis_Viirpalu <talis.viirpalu@wisercat.net>
2022-12-05 16:34:21 +0200

    updated banner content with gifs

--- src/app/PAGES/reference/reference.component.ts
+++ src/app/PAGES/reference/reference.component.ts
@@ -3,46 +3,65 @@ import {Component, HostListener, OnInit, ViewChild} from '@angular/core';
 @Component({
   selector: 'app-reference',
   template: `
-  <div *ngIf="!modalOpen" class="container-fluid">
+    <div *ngIf="!modalOpen" class="container-fluid">
 
-    <!-- header -->
+      <!-- header -->
 
-<!--    <app-header></app-header>-->
+      <!--    <app-header></app-header>-->
 
-    <!-- sidebar test -->
+      <!-- sidebar test -->
 
-    <div id="nav" class="container-fluid">
-      <span (click)="scrollTop()">home</span>
-      <span (click)="scroll(services)">services</span>
-      <span (click)="scroll(about)">about</span>
-<!--      <span (click)="scroll(showreel)">showreel</span>-->
-      <span (click)="scroll(contacts)">contacts</span>
-    </div>
+      <div id="nav" class="container-fluid">
+        <span (click)="scrollTop()">home</span>
+        <span (click)="scroll(services)">services</span>
+        <span (click)="scroll(about)">about</span>
+        <!--      <span (click)="scroll(showreel)">showreel</span>-->
+        <span (click)="scroll(contacts)">contacts</span>
+      </div>
 
       <!-- promo clip -->
 
-    <div class="container-fluid video_container">
-      <div class="video-wrapper">
-        <div class="overlay"></div>
-        <div class="overlay2"></div>
-        <div #overlay id="overlayScroll" [class.bg]="bgScroll"></div>
-        <img src="../../../assets/giphy.gif" class="overlay3">
+      <div class="container-fluid video_container">
+        <div class="video-wrapper">
+          <!--        <div class="overlay"></div>-->
+          <div class="overlay2"></div>
+          <div #overlay id="overlayScroll" [class.bg]="bgScroll"></div>
+          <!--        <img src="../../../assets/banner.gif" class="overlay3">-->
 
-        <section>
-          <a (click)="modalOpen = true"
-             class="playBtn"></a>
-        </section>
+          <div class="container gif-container ">
+            <!--        <div class="overlay"></div>-->
+
+            <div class="row-img">
+              <!--            <div class="gif-overlay-fx"></div>-->
+              <img src="../../../assets/banner.gif">
+            </div>
+            <div class="row-img">
+              <div class="col">
+                <img src="../../../assets/banner4.gif">
+              </div>
+            </div>
+            <div class="row-img">
+              <div class="col">
+                <img src="../../../assets/banner3.gif">
+              </div>
+            </div>
+          </div>
+
+          <section id="banner-play-button">
+            <a (click)="modalOpen = true"
+               class="playBtn"></a>
+          </section>
+        </div>
       </div>
-    </div>
 
-    <!-- SERVICES -->
+      <!-- SERVICES -->
 
-    <div #services id="services" class="container d-flex">
-      <div class="container py-5">
-        <h1>SERVICES</h1>
+      <div #services id="services" class="container d-flex">
+        <div class="container py-5">
+          <h1>SERVICES</h1>
 
-        <div id="services_container"
-             class="container">
+          <div id="services_container"
+               class="container">
             <div class="container services">
               <div class="titleText_container">
                 <h3>🎬 Eelproduktsioon</h3>
@@ -75,109 +94,109 @@ import {Component, HostListener, OnInit, ViewChild} from '@angular/core';
                 <p class="tags">Editing // Color grading // Sound design // VFX</p>
               </div>
             </div>
-        </div>
-      </div>
-    </div>
-
-    <!-- about -->
-
-    <div #about id="about" class="container">
-      <div class="container py-5">
-        <h1>ABOUT</h1>
-
-        <div id="about_container"
-             class="container d-flex flex-xxl-row flex-sm-column">
-          <div id="about_img"
-               class="container mx-auto">
-            <!-- <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/STS-87_crew_1.jpg/1200px-STS-87_crew_1.jpg" class="rounded float-start" alt="..."></div> -->
-            <img src="../../../assets/tv%20(1).svg" class="rounded float-start"  alt="">
-            <div id="about_text"
-                 class="container">
-              <p>Oleme kolm loomingulist Eesti filmitegijat, kelle kireks on liikuvate piltide abil ideid ellu viia. Meie tiimil on põhjalikud ja pikaajalised teadmised Eesti filmi- ja teletööstusest. Olles ka aktiivsed sotsiaalmeedia kasutajad, teame, kuidas hoida teie ettevõtte sotsiaalmeedia video- ja pildimaterjal asjakohasena.</p>
-            </div>
           </div>
         </div>
       </div>
-    </div>
 
-    <!-- contacts -->
+      <!-- about -->
 
-    <div #contacts id="contacts" class="container">
-      <div class="container py-5">
-        <h1>CONTACTS</h1>
+      <div #about id="about" class="container">
+        <div class="container py-5">
+          <h1>ABOUT</h1>
 
-        <div id="contacts_container"
-             class="container flex-sm-column flex-md-row flex-lg-row flex-xl-row flex-xxl-row justify-content-center align-items-center">
-
-          <div class="card mx-auto" style="width: 18rem;">
-            <img src="../../assets/_DSC4905.jpg" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h4>ALEX TERVINSKY</h4>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <p>test@mail.com</p>
-              <p>58505555</p>
-            </div>
-          </div>
-
-          <div class="card mx-auto" style="width: 18rem;">
-            <img src="../../assets/_DSC4909.jpg"
-                 class="card-img-top" alt="...">
-            <div class="card-body">
-              <h4>SIMMO SASKA</h4>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <p>test@mail.com</p>
-              <p>58505555</p>
-            </div>
-          </div>
-
-          <div class="card mx-auto" style="width: 18rem;">
-            <img src="../../assets/_DSC4930.jpg" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h4>TOOMAS TATAR</h4>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <p>test@mail.com</p>
-              <p>58505555</p>
+          <div id="about_container"
+               class="container d-flex flex-xxl-row flex-sm-column">
+            <div id="about_img"
+                 class="container mx-auto">
+              <!-- <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/STS-87_crew_1.jpg/1200px-STS-87_crew_1.jpg" class="rounded float-start" alt="..."></div> -->
+              <img src="../../../assets/tv%20(1).svg" class="rounded float-start"  alt="">
+              <div id="about_text"
+                   class="container">
+                <p>Oleme kolm loomingulist Eesti filmitegijat, kelle kireks on liikuvate piltide abil ideid ellu viia. Meie tiimil on põhjalikud ja pikaajalised teadmised Eesti filmi- ja teletööstusest. Olles ka aktiivsed sotsiaalmeedia kasutajad, teame, kuidas hoida teie ettevõtte sotsiaalmeedia video- ja pildimaterjal asjakohasena.</p>
+              </div>
             </div>
           </div>
         </div>
       </div>
+
+      <!-- contacts -->
+
+      <div #contacts id="contacts" class="container">
+        <div class="container py-5">
+          <h1>CONTACTS</h1>
+
+          <div id="contacts_container"
+               class="container flex-sm-column flex-md-row flex-lg-row flex-xl-row flex-xxl-row justify-content-center align-items-center">
+
+            <div class="card mx-auto" style="width: 18rem;">
+              <img src="../../assets/_DSC4905.jpg" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h4>ALEX TERVINSKY</h4>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <p>test@mail.com</p>
+                <p>58505555</p>
+              </div>
+            </div>
+
+            <div class="card mx-auto" style="width: 18rem;">
+              <img src="../../assets/_DSC4909.jpg"
+                   class="card-img-top" alt="...">
+              <div class="card-body">
+                <h4>SIMMO SASKA</h4>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <p>test@mail.com</p>
+                <p>58505555</p>
+              </div>
+            </div>
+
+            <div class="card mx-auto" style="width: 18rem;">
+              <img src="../../assets/_DSC4930.jpg" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h4>TOOMAS TATAR</h4>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <p>test@mail.com</p>
+                <p>58505555</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- footer -->
+
+      <footer id="footer" class="container">
+        <!--                  logo-->
+        <!--      <div id="footer_logo" class="container">-->
+        <!--        <img src="../../../assets/LOGO-HOW-BZZAR.svg" alt="">-->
+        <!--      </div>-->
+
+        <!-- aadress     -->
+        <div id="address" class="container">
+          <h1>Marati 5,</h1>
+          <h1>11713 Tallinn</h1>
+        </div>
+        <!-- kontaktid     -->
+        <div id="footer_contacts" class="container">
+          <span><strong>TELEFON: </strong>+372 58316922</span>
+          <span><strong>EMAIL: </strong>test@test.ee</span>
+          <br>
+          <a href="http://www.instagram.com">INSTAGRAM</a>
+          <a href="http://www.facebook.com">FACEBOOK</a>
+          <a href="http://www.vimeo.com">VIMEO</a>
+        </div>
+      </footer>
+
     </div>
-
-    <!-- footer -->
-
-    <footer id="footer" class="container">
-            <!--      logo-->
-<!--      <div id="footer_logo" class="container">-->
-<!--        <img src="../../../assets/LOGO-HOW-BZZAR.svg" alt="">-->
-<!--      </div>-->
-
-            <!-- aadress     -->
-      <div id="address" class="container">
-        <h1>Marati 5,</h1>
-        <h1>11713 Tallinn</h1>
-      </div>
-            <!-- kontaktid     -->
-      <div id="footer_contacts" class="container">
-        <span><strong>TELEFON: </strong>+372 58316922</span>
-        <span><strong>EMAIL: </strong>test@test.ee</span>
-        <br>
-        <a href="http://www.instagram.com">INSTAGRAM</a>
-        <a href="http://www.facebook.com">FACEBOOK</a>
-        <a href="http://www.vimeo.com">VIMEO</a>
-      </div>
-    </footer>
-
-  </div>
 
     <!-- Video Modal -->
 
-  <div *ngIf="modalOpen"
-       class="container-fluid modal">
-    <div class="btn_container">
-      <span id="exitButton" (click)="test()">X</span>
+    <div *ngIf="modalOpen"
+         class="container-fluid modal">
+      <div class="btn_container">
+        <span id="exitButton" (click)="test()">X</span>
+      </div>
+      <iframe id="video" title="vimeo-player" src="https://player.vimeo.com/video/755876584?h=ca792f1001" frameborder="0" allowfullscreen></iframe><!--        <iframe id="video"-->
     </div>
-    <iframe id="video" title="vimeo-player" src="https://player.vimeo.com/video/755876584?h=ca792f1001" frameborder="0" allowfullscreen></iframe><!--        <iframe id="video"-->
-  </div>
   `,
   styleUrls: ['./reference.component.scss']
 })
